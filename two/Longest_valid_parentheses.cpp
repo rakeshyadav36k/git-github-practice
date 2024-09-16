966db041b15c9001fd43c87fa18c948156018186
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-09-16 22:42:04 +0530

    updated

--- two/Longest_valid_parentheses.cpp
+++ two/Longest_valid_parentheses.cpp
@@ -1,6 +1,13 @@
+/*
+Given a string str consisting of opening and closing parenthesis '(' and ')'. Find length of the longest valid parenthesis substring.
+A parenthesis string is valid if:
+->For every opening parenthesis, there is a closing parenthesis.
+->Opening parenthesis must be closed in the correct order.
+
+ */
+
 #include<bits/stdc++.h>
 using namespace std;
-
 
 class Solution {
   public:
