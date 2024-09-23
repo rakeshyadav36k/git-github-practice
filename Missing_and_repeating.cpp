a4bf604e6c910281277c851334f0fa3a5ab5a2e9
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-09-23 22:12:53 +0530

    Missing and repeating

---
+++ Missing_and_repeating.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        int  i = 0;
+        int repeated = -1;
+        int missing = -1;
+        
+        vector<int> ans(2);
+        for(int i = 0; i < arr.size(); i++){
+            repeated = abs(arr[i]);
+            missing = arr[repeated-1];
+            if(missing > 0){
+                arr[repeated-1] = -missing;
+            }
+            else ans[0] = repeated;
+        }
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] > 0){
+                ans[1] = i+1;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
