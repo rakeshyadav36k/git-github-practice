3826f422d2ca8649288a4fe9f9fe0fd9f36c3627
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-09-29 22:49:59 +0530

    code of the problem

--- two/Total_count.cpp
+++ two/Total_count.cpp
@@ -4,3 +4,21 @@ You are given an array arr[] of positive integers and a threshold value k.
  such that each divided integer is less than or equal to k.
  Compute the total number of these integer across all elements of the array.
 */
+
+class Solution {
+  public:
+    int totalCount(int k, vector<int>& arr) {
+        int n = arr.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] % k == 0)
+                ans += arr[i] / k;
+            
+            else
+                ans += arr[i] / k + 1;
+            
+        }
+        
+        return ans;
+    }
+};
