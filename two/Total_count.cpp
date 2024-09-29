1486ab2ef7adbf9711bb3a20326b861805fa3bbb
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-09-29 22:49:03 +0530

    Total Count

---
+++ two/Total_count.cpp
@@ -0,0 +1,6 @@
+/*
+You are given an array arr[] of positive integers and a threshold value k.
+ For each element in the array, divide it into the minimum number of small integers 
+ such that each divided integer is less than or equal to k.
+ Compute the total number of these integer across all elements of the array.
+*/
