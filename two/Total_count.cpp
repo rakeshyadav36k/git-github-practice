a83281721b645d448d30d32a1dc99ce25792373b
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-09-29 22:51:07 +0530

    final code

--- two/Total_count.cpp
+++ two/Total_count.cpp
@@ -5,6 +5,16 @@ You are given an array arr[] of positive integers and a threshold value k.
  Compute the total number of these integer across all elements of the array.
 */
 
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
 class Solution {
   public:
     int totalCount(int k, vector<int>& arr) {
@@ -22,3 +32,30 @@ class Solution {
         return ans;
     }
 };
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.totalCount(k, arr);
+        cout << res << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
