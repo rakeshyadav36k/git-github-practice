13578d85f79d5cc2ce7acc940441fc1cfbe6db8c
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-10-06 18:56:14 +0530

    code

--- two/Find_the_no_of_islands.cpp
+++ two/Find_the_no_of_islands.cpp
@@ -6,3 +6,52 @@ Note: An island is either surrounded by water or the boundary of a grid and is f
 in all 8 directions.
 */
 
+
+class Solution {
+  public:
+    void bfs(int row,int col,vector<vector<char>>& grid,vector<vector<int>> &vis){
+        int n=grid.size();
+        int m=grid[0].size();
+        vis[row][col]=1;
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        while(!q.empty())
+        {
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int delrow=-1;delrow<=1;delrow++)
+            {
+                for(int delcol=-1;delcol<=1;delcol++)
+                {
+                    int nrow=row+delrow;
+                    int ncol=col+delcol;
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
+                        vis[nrow][ncol]=1;
+                        q.push({nrow,ncol});
+                    }
+                }
+            }
+        }
+    }
+    
+    // Function to find the number of islands.
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int count=0;
+        for(int row=0;row<n;row++)
+        {
+            for(int col=0;col<m;col++)
+            {
+                if(!vis[row][col] && grid[row][col]=='1')
+                {
+                    count++;
+                    bfs(row,col,grid,vis);
+                }
+            }
+        }
+        return count;
+    }
+};
