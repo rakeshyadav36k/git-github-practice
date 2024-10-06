f0477b4d040513d392f55352466764f07e7157a5
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-10-06 18:51:41 +0530

    final update

--- two/Remove_methods_from_projects.cpp
+++ two/Remove_methods_from_projects.cpp
@@ -15,6 +15,76 @@ If it is not possible to remove all the suspicious methods, none should be remov
 
 class Solution {
 public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        // Step 1: Build the adjacency list for the invocation relationships
+        vector<vector<int>> adj(n);
+        for (const auto& invocation : invocations) {
+            adj[invocation[0]].push_back(invocation[1]);
+        }
+
+        // Step 2: Perform BFS to find all methods that are suspicious (invoked by or invoking method k)
+        unordered_set<int> suspicious = findSuspiciousMethods(n, k, adj);
+
+        // Step 3: Check if any suspicious method is invoked by methods outside the suspicious set
+        if (hasExternalInvocation(n, suspicious, adj)) {
+            // If there's an external invocation, return all methods
+            vector<int> result(n);
+            iota(result.begin(), result.end, 0);  // Fills result with 0, 1, 2, ..., n-1
+            return result;
+        } else {
+            // Otherwise, return only non-suspicious methods
+            vector<int> result;
+            for (int i = 0; i < n; i++) {
+                if (suspicious.find(i) == suspicious.end()) {
+                    result.push_back(i);
+                }
+            }
+            return result;
+        }
+    }
+
+private:
+    // Helper function to perform BFS and find all suspicious methods
+    unordered_set<int> findSuspiciousMethods(int n, int k, const vector<vector<int>>& adj) {
+        unordered_set<int> suspicious;
+        queue<int> q;
+        q.push(k);
+        suspicious.insert(k);
+
+        while (!q.empty()) {
+            int method = q.front();
+            q.pop();
+
+            // Traverse all methods invoked by the current method
+            for (int invokedMethod : adj[method]) {
+                if (suspicious.find(invokedMethod) == suspicious.end()) {
+                    suspicious.insert(invokedMethod);
+                    q.push(invokedMethod);
+                }
+            }
+        }
+        return suspicious;
+    }
+
+    // Helper function to check if there is an external invocation
+    bool hasExternalInvocation(int n, const unordered_set<int>& suspicious, const vector<vector<int>>& adj) {
+        for (int i = 0; i < n; i++) {
+            if (suspicious.find(i) == suspicious.end()) {
+                // Check if a non-suspicious method invokes any suspicious method
+                for (int invokedMethod : adj[i]) {
+                    if (suspicious.find(invokedMethod) != suspicious.end()) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+/*
+class Solution {
+public:
     bool hasExternalInvocation(int n, const unordered_set<int>& suspicious, const vector<vector<int>>& adj) {
         for (int i = 0; i < n; i++) {
             if (suspicious.find(i) == suspicious.end()) {
@@ -72,3 +142,4 @@ public:
         }
     }
 };
+*/
