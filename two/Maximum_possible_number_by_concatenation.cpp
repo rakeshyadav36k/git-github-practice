2e8b693761a7bea201a72dc1af83a10c90d99a91
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-10-06 18:27:52 +0530

    final code

--- two/Maximum_possible_number_by_concatenation.cpp
+++ two/Maximum_possible_number_by_concatenation.cpp
@@ -5,3 +5,36 @@ Return the maximum possible number whose binary representation can be formed by 
 
 Note that the binary representation of any number does not contain leading zeros.
  */
+
+class Solution {
+public:
+    string toBinary(int num) {
+        string binary = "";
+        while (num > 0) {
+            binary = (num % 2 == 0 ? "0" : "1") + binary;
+            num /= 2;
+        }
+        return binary;
+    }
+    
+    
+    static bool cmp(int a, int b){
+        Solution sol;
+        string binA = sol.toBinary(a);
+        string binB = sol.toBinary(b);
+        return (binA + binB) > (binB + binA);
+    }
+    
+    int maxGoodNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), cmp);
+        
+        string concatenatedBinary = "";
+        for (int num : nums) {
+            concatenatedBinary += toBinary(num);
+        }
+        
+        int result = stoi(concatenatedBinary, 0, 2);
+        
+        return result;
+    }
+};
