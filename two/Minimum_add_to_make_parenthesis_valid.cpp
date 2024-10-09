d3c5354a1d022fc06b0863ac03555bc787219248
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-10-09 22:40:22 +0530

    using stack

--- two/Minimum_add_to_make_parenthesis_valid.cpp
+++ two/Minimum_add_to_make_parenthesis_valid.cpp
@@ -11,3 +11,24 @@ Return the minimum number of moves required to make s valid.
  */
 
 // 1st method using stack
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n = s.size();
+        stack<char> st;
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == ')'){
+                if(st.empty() || st.top() == ')')
+                    st.push(s[i]);
+
+                else if(!st.empty() && st.top() == '(')
+                    st.pop();
+            }
+
+            else st.push(s[i]);
+        }
+
+        return st.size();
+    }
+};
