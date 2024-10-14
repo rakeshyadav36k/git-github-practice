34b3413b14e357985f4831f7772e609bb9733149
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-10-14 19:37:02 +0530

    code of the combination sum

--- DSA/combination_sum.cpp
+++ DSA/combination_sum.cpp
@@ -28,3 +28,35 @@ Example 3:
 Input: candidates = [2], target = 1
 Output: []
  */
+
+class Solution {
+public:
+    void combSum(int ind,vector<int> arr,vector<int> &temp,
+        vector<vector<int>> &ans,int &target,int n)
+    {
+        if(ind==n){
+            if(target==0){
+                ans.push_back(temp);
+            }
+            return;
+        }
+        // pick up the same element
+        if(arr[ind]<=target){
+            target -= arr[ind];
+            temp.push_back(arr[ind]);
+            combSum(ind,arr,temp,ans,target,n);
+            target += arr[ind];
+            temp.pop_back();
+        }
+
+        combSum(ind+1,arr,temp,ans,target,n);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        int n = candidates.size();
+        combSum(0,candidates,temp,ans,target,n);
+
+        return ans;
+    }
+};
