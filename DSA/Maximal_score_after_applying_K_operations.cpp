46dc6ec0670f22eecdc47ed59ad10df55beafa83
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-10-14 19:31:45 +0530

    final code

--- DSA/Maximal_score_after_applying_K_operations.cpp
+++ DSA/Maximal_score_after_applying_K_operations.cpp
@@ -31,5 +31,24 @@ public:
 };
 
 int main (){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> vec(n);
+        for(int i=0; i<n; i++){
+            cin >> vec[i];
+        }
+
+        int k;
+        cin >> k;
+
+        Solution ob;
+         cout << ob.maxKelements(vec, k) << endl;
+    }
+
     return 0;
 }
