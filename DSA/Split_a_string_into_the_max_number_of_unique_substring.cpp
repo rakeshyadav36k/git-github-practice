ecf9af14c6c3f1922cb2a82d3161eacb72941fb4
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-10-21 19:52:45 +0530

    code

--- DSA/Split_a_string_into_the_max_number_of_unique_substring.cpp
+++ DSA/Split_a_string_into_the_max_number_of_unique_substring.cpp
@@ -6,3 +6,33 @@ You can split string s into any list of non-empty substrings, where the concaten
 
 A substring is a contiguous sequence of characters within a string.
  */
+
+class Solution {
+public:
+
+    void solve(int i, string s,unordered_set<string> &st, int currCnt, int &maxCnt){
+        if(i >= s.length()){
+            maxCnt = max(currCnt, maxCnt);
+            return;
+        }
+
+        for(int j = i; j < s.length(); j++){
+            string sub = s.substr(i, j-i+1);
+            if(st.find(sub) == st.end()){
+                st.insert(sub);
+                solve(j+1, s, st, currCnt+1, maxCnt);
+                st.erase(sub);
+            }
+        }
+    }
+
+    int maxUniqueSplit(string s) {
+        unordered_set<string> st;
+        int maxCnt = 0;
+
+        int i = 0;
+        solve(i, s, st, 0, maxCnt);
+
+        return maxCnt;
+    }
+};
