aac4025ec74f9db5f9e35ed2eb5c931a48c78246
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-11-24 22:05:50 +0530

    code

--- DSA/kadane_algo.cpp
+++ DSA/kadane_algo.cpp
@@ -1,5 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int maxSubarraySum(vector<int> &arr) {
+        int n = arr.size();
+        int currSum = 0, maxSum = INT_MIN;
+        for(int i = 0; i < n; i++){
+            currSum += arr[i];
+            maxSum = max(maxSum, currSum);
+            
+            if(currSum < 0) currSum = 0;
+        }
+        
+        return maxSum;
+    }
+
 int main(){
     
     return 0;
