803607dfac62ff2014f5e3fdb09aac66f7ff9db8
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-03 23:02:03 +0530

    using LPS

--- GFG_daily_problem/01_min_chars_to_add_for_palindrome.cpp
+++ GFG_daily_problem/01_min_chars_to_add_for_palindrome.cpp
@@ -3,3 +3,47 @@
 
 Note: A palindrome string is a sequence of characters that reads the same forward and backward.
  */
+
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minChar(string& s) {
+        string rev = s; reverse(rev.begin(), rev.end());
+        string concat = s + "#" + rev;
+        int n = concat.size(), l = 0;
+        vector<int> lps(n, 0);
+        for (int i = 1; i < n; i++) {
+            while (l > 0 && concat[i] != concat[l]) l = lps[l - 1];
+            if (concat[i] == concat[l]) l++;
+            lps[i] = l;
+        }
+        return s.size() - lps[n - 1];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.minChar(str);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
