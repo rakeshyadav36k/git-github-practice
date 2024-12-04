41347a5e2fa16d118ab5938ae84e3e0bf0053c82
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-04 22:52:40 +0530

    code

---
+++ GFG_daily_problem/02_strings_relation_of_eachothers.cpp
@@ -0,0 +1,50 @@
+// You are given two strings of equal lengths, s1 and s2. The task is to check if s2 is a rotated version of the string s1.
+
+// Note: The characters in the strings are in lowercase.
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string &s1, string &s2) {
+        // Your code here
+        if (s1.size() != s2.size()) {
+            return false;
+        }
+
+        string temp = s1 + s1;
+        return (temp.find(s2) != string::npos);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        Solution sol;
+        int result = sol.areRotations(s1, s2);
+        if (result) {
+            cout << "true" << endl;
+
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
