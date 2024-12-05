470f51bd86f0262a7823fcfc015b52c7351c9604
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-05 19:03:00 +0530

    brute force

--- Leetcode_daily_problem/03_move_pieces_to_obtain_a_string.cpp
+++ Leetcode_daily_problem/03_move_pieces_to_obtain_a_string.cpp
@@ -6,3 +6,39 @@ and a piece 'R' can move to the right only if there is a blank space directly to
 The character '_' represents a blank space that can be occupied by any of the 'L' or 'R' pieces.
 Return true if it is possible to obtain the string target by moving the pieces of the string start any number of times. Otherwise, return false.
  */
+
+
+
+// ---------- Brute force- it gives TLE
+class Solution {
+public:
+    bool solve(string &str, string &target, unordered_map<string, bool> &mp, int n){
+        if(str == target) return true;
+
+        if(mp.count(str)) return mp[str];
+
+        for(int i = 0; i < n; i++){
+            if(str[i] == 'L' && i > 0 && str[i-1] == '_'){
+                swap(str[i], str[i-1]);
+                if(solve(str, target, mp, n) == true){
+                    return true;
+                }
+                swap(str[i], str[i-1]);
+            }
+            else if(str[i] == 'R' && i < n-1 && str[i+1] == '_'){
+                swap(str[i], str[i+1]);
+                if(solve(str, target, mp, n) == true){
+                    return true;
+                }
+                swap(str[i], str[i+1]);
+            }
+        }
+        return mp[str] = false;
+    }
+
+    bool canChange(string start, string target) {
+        unordered_map<string, bool> mp;
+        int n = start.size();
+        return solve(start, target, mp, n);
+    }
+};
