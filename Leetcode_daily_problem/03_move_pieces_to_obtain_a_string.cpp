cda3a0632c860635c42ecc1ba4441d4105736896
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-05 19:04:12 +0530

    two pointer method

--- Leetcode_daily_problem/03_move_pieces_to_obtain_a_string.cpp
+++ Leetcode_daily_problem/03_move_pieces_to_obtain_a_string.cpp
@@ -42,3 +42,32 @@ public:
         return solve(start, target, mp, n);
     }
 };
+
+
+// --- using two pointer method
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n = start.size();
+        int i = 0;  // for start
+        int j = 0;  // for target
+
+        while(i < n || j < n){
+            while(i < n && start[i] == '_') i++;
+            while(j < n && target[j] == '_') j++;
+
+            if(i == n || j == n){
+                return i == n && j == n;
+            }
+
+            if(start[i] != target[j]) return false;
+            if(start[i] == 'L' && i < j) return false;
+            if(start[i] == 'R' && i > j) return false;
+
+            i++;
+            j++;
+        }
+
+        return true;
+    }
+};
