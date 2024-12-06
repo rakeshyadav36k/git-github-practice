cbc1c79c31f12dd65ef00b418c7bb0e10c92fafe
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-06 20:39:54 +0530

    using set

--- Leetcode_daily_problem/04_Maximum_number_of_integer_choose_from_range.cpp
+++ Leetcode_daily_problem/04_Maximum_number_of_integer_choose_from_range.cpp
@@ -7,3 +7,22 @@ The chosen integers should not be in the array banned.
 The sum of the chosen integers should not exceed maxSum.
 Return the maximum number of integers you can choose following the mentioned rules.
  */
+
+// ---------- using set ---------- TC - O(n)  SC - (m)
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        unordered_set<int> st(banned.begin(), banned.end());
+
+        int sum = 0;
+        int cnt = 0;
+        for(int i = 1; i <= n; i++){
+            if(st.find(i) == st.end()){
+                sum += i;
+                if(sum <= maxSum) cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
