11aa59487235febcdfbeb3f061953e28046c8e81
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-06 20:43:25 +0530

    find H index

---
+++ GFG_daily_problem/04_Find_H-index.cpp
@@ -0,0 +1,5 @@
+/**
+ * Given an integer array citations[], where citations[i] is the number of citations a researcher received for the ith paper. The task is to find the H-index.
+
+H-Index is the largest value such that the researcher has at least H papers that have been cited at least H times.
+ */
