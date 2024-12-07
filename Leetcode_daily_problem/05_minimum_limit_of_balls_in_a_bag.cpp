b251d798750717a01b1d51492d184ec7f4f2a011
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-07 19:57:03 +0530

    binary search

--- Leetcode_daily_problem/05_minimum_limit_of_balls_in_a_bag.cpp
+++ Leetcode_daily_problem/05_minimum_limit_of_balls_in_a_bag.cpp
@@ -9,3 +9,31 @@ Your penalty is the maximum number of balls in a bag. You want to minimize your 
 
 Return the minimum possible penalty after performing the operations.
  */
+
+class Solution {
+public:
+
+    bool check(int mid, vector<int> &nums, int maxOperations) {
+        int n = nums.size(); 
+        for (auto &num: nums) {
+            maxOperations -= (num + mid - 1) / mid - 1;
+        }
+        return maxOperations >= 0; 
+    }
+
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int n = nums.size(); 
+        int l = 1, r = 1e9; 
+        int ans = 1e9; 
+        while (l <= r) {
+            int mid = l + (r - l) / 2; 
+            if (check(mid, nums, maxOperations)) {
+                ans = mid; 
+                r = mid - 1; 
+            } else {
+                l = mid + 1; 
+            }
+        }
+        return ans; 
+    }
+};
