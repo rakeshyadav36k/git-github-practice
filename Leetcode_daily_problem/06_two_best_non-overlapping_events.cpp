59d96030aab50529f1aa1a19e0222ab57a492c2d
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-08 21:10:35 +0530

    greedy algo

--- Leetcode_daily_problem/06_two_best_non-overlapping_events.cpp
+++ Leetcode_daily_problem/06_two_best_non-overlapping_events.cpp
@@ -10,6 +10,9 @@ you cannot attend two events where one of them starts and the other ends at the 
 More specifically, if you attend an event with end time t, the next event must start at or after t + 1.
  */
 
+
+// ------- sorting + binary search
+
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
@@ -52,3 +55,36 @@ public:
         return maxSum;
     }
 };
+
+
+
+
+
+// ------------- sorting + greedy
+class Solution {
+public:
+    using info=tuple<int, bool, int>;
+    static int maxTwoEvents(vector<vector<int>>& events) {
+        const int n=events.size();
+        vector<info> time(n*2);
+        for(int i=0; i<n; i++){
+            int s=events[i][0], e=events[i][1], v=events[i][2];
+            time[2*i]={s, 0, v};
+            time[2*i+1]={e, 1, v};
+        }
+        sort(time.begin(), time.end());
+        int ans=0, maxV=0, n2=n*2;
+        for(auto& [t, isEnd, v]: time){
+            if (isEnd) maxV=max(maxV, v);
+            else ans=max(ans, maxV+v);
+        }
+        return ans;    
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
