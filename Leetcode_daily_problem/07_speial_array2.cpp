9c8cd977acbe64e4e20e28cbd2feb404f35915f9
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-09 23:48:49 +0530

    using prefix sum

--- Leetcode_daily_problem/07_speial_array2.cpp
+++ Leetcode_daily_problem/07_speial_array2.cpp
@@ -7,3 +7,31 @@ subarray
 
 Return an array of booleans answer such that answer[i] is true if nums[fromi..toi] is special.
  */
+
+// ------------ using prefix sum
+
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> prefixSumOfValid(nums.size(), 0);
+        int cnt = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i]%2 == nums[i-1]%2){
+                cnt++;
+            }
+            prefixSumOfValid[i] = cnt;
+        }
+        
+        vector<bool> ans;
+        for(auto &range : queries){
+            int l = range[0];
+            int r = range[1];
+            if(prefixSumOfValid[r] - prefixSumOfValid[l] == 0){
+                ans.push_back(true);
+            }
+            else ans.push_back(false);
+        }
+
+        return ans;
+    }
+};
