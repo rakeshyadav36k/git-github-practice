f6a7a4ab382f593d33875fb979524dd6be57de56
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-10 20:38:29 +0530

    Non-overlapping intervals

---
+++ GFG_daily_problem/08_Non-overlapping_intervals.cpp
@@ -0,0 +1,4 @@
+/**
+ * Given a 2D array intervals[][] of representing intervals where intervals [i] = [starti, endi ]. 
+ * Return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+ */
