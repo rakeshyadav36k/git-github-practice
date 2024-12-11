38b9cfd0265a86adcc4665ccbf80d490f17fc8bf
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-11 20:40:06 +0530

    merge without extra space

---
+++ GFG_daily_problem/09_merge_without_extra_space.cpp
@@ -0,0 +1,5 @@
+/**
+ * Given two sorted arrays a[] and b[] of size n and m respectively, 
+ * the task is to merge them in sorted order without using any extra space. 
+ * Modify a[] so that it contains the first n elements and modify b[] so that it contains the last m elements.
+ */
