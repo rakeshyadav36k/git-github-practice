64330447d9a3afe2a2117cf112d8b6440a3cea9e
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-14 20:23:40 +0530

    binary search method

--- GFG_daily_problem/11_search_in_a_rotated_sorted_array.cpp
+++ GFG_daily_problem/11_search_in_a_rotated_sorted_array.cpp
@@ -18,13 +18,16 @@ public:
             if(arr[mid] == key) return mid;
             
             
-            // Checking whether the left half is sorted or not
-            if(arr[mid] >= arr[low]) {
+         
+            if(arr[mid] >= arr[low]) { // if it is true it means left half is sorted
+
                 // Checking whether key lies beteen low and mid or not
                 if(key>=arr[low] && key<=arr[mid]) high = mid-1;
                 else low = mid+1;
             } 
-            else {
+
+            else { // if it is true it means right half is sorted
+
                 // Checking whether key lies between mid and high or not
                 if(key>=arr[mid] && key<=arr[high]) low = mid+1;
                 else high = mid-1;
