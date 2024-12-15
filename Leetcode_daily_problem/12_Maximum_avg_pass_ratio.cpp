cc638ba5da3c663614e35b648235a2672f4d4903
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-15 19:53:17 +0530

    priority queue

--- Leetcode_daily_problem/12_Maximum_avg_pass_ratio.cpp
+++ Leetcode_daily_problem/12_Maximum_avg_pass_ratio.cpp
@@ -10,3 +10,36 @@ The average pass ratio is the sum of pass ratios of all the classes divided by t
 
 Return the maximum possible average pass ratio after assigning the extraStudents students. Answers within 10-5 of the actual answer will be accepted.
  */
+
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        auto gain = [](double pass, double total) {
+            return (pass + 1) / (total + 1) - pass / total;
+        };
+
+        priority_queue<pair<double, pair<int, int>>> maxHeap;
+
+        double sum = 0.0;
+
+        for (const auto& cls : classes) {
+            int pass = cls[0], total = cls[1];
+            sum += (double)pass / total;  
+            maxHeap.push({gain(pass, total), {pass, total}});
+        }
+
+        for (int i = 0; i < extraStudents; ++i) {
+            auto [currentGain, data] = maxHeap.top(); maxHeap.pop();
+            int pass = data.first, total = data.second;
+
+            sum -= (double)pass / total;
+            pass += 1;
+            total += 1;
+            sum += (double)pass / total;
+
+            maxHeap.push({gain(pass, total), {pass, total}});
+        }
+
+        return sum / classes.size();
+    }
+};
