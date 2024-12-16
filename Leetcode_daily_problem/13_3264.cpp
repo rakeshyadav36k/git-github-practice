a61d3379deb30f2c5f6db879831efc3461a6b51c
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-16 18:18:41 +0530

    priority queue

--- Leetcode_daily_problem/13_3264.cpp
+++ Leetcode_daily_problem/13_3264.cpp
@@ -7,3 +7,28 @@ Find the minimum value x in nums. If there are multiple occurrences of the minim
 Replace the selected minimum value x with x * multiplier.
 Return an integer array denoting the final state of nums after performing all k operations.
  */
+
+class Solution {
+public:
+    #define P pair<int, int> 
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        int n = nums.size();
+        priority_queue<P, vector<P>, greater<P>> pq;
+
+        for(int i = 0; i < n; i++){
+            pq.push({nums[i], i});
+        }
+
+        while(k--){
+            int num = pq.top().first;
+            int idx = pq.top().second;
+            pq.pop();
+
+            nums[idx] = multiplier*num;
+
+            pq.push({multiplier * num, idx});
+        }
+
+        return nums;
+    }
+};
