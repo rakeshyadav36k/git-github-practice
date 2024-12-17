13cb4fd2cec76fc88e21a39865a9b5b3c6de6ff6
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-17 21:11:41 +0530

    two pointer

--- Leetcode_daily_problem/13_2182.cpp
+++ Leetcode_daily_problem/13_2182.cpp
@@ -9,3 +9,41 @@ A string a is lexicographically larger than a string b if in the first position 
 string a has a letter that appears later in the alphabet than the corresponding letter in b. If the first min(a.length, b.length) characters do not differ, 
 then the longer string is the lexicographically larger one.
  */
+
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        sort(s.rbegin(), s.rend());
+
+        string result;
+        int freq = 1;
+        int pointer = 0;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (!result.empty() && result.back() == s[i]) {
+                if (freq < repeatLimit) {
+                    result += s[i];
+                    freq++;
+                } else {
+                    pointer = max(pointer, i + 1);
+                    while (pointer < s.size() && s[pointer] == s[i]) {
+                        pointer++;
+                    }
+
+                    if (pointer < s.size()) {
+                        result += s[pointer];
+                        swap(s[i], s[pointer]);
+                        freq = 1;
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                result += s[i];
+                freq = 1;
+            }
+        }
+
+        return result;                                                           
+    }
+};
