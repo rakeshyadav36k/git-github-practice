bfe94b318ec913801191a254c0ff787e8aa4cf88
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-19 19:19:03 +0530

    using prefix sum

--- Leetcode_daily_problem/16_max_chunks_to_make_sorted.cpp
+++ Leetcode_daily_problem/16_max_chunks_to_make_sorted.cpp
@@ -7,3 +7,27 @@ We split arr into some number of chunks (i.e., partitions), and individually sor
 
 Return the largest number of chunks we can make to sort the array.
  */
+
+// ----------- using prefix sum
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        
+        int sortedPrefixSum = 0;  // prefix sum of sorted array -> 0 1 2 3 4 5 6 7 ......
+        int prefixSum = 0;
+        int cnt = 0;   // no of chunks
+        for(int i = 0; i < n; i++){    
+            sortedPrefixSum += i;
+
+            prefixSum += arr[i];
+
+            if(prefixSum == sortedPrefixSum){
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
