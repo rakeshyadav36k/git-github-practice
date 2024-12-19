45b7f8bdee2fdb7f4261c31d24c4de454305e0e2
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-20 01:40:39 +0530

    code

--- GFG_daily_problem/17_spirally_traversing_a_matrix.cpp
+++ GFG_daily_problem/17_spirally_traversing_a_matrix.cpp
@@ -2,3 +2,72 @@
  * You are given a rectangular matrix mat[][] of size n x m, 
  * and your task is to return an array while traversing the matrix in spiral form.
  */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> ans;
+        int top = 0, left = 0;
+        int bottom = m-1, right = n-1;
+
+        while(left <= right && top <= bottom){
+            for(int i=left;i<=right;i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if(top <= bottom){
+                for(int i=right;i>=left;i--){
+                    ans.push_back(matrix[bottom][i]);
+                }
+            bottom--;
+            }
+            if(left <= right){
+                for(int i=bottom;i>=top;i--){
+                    ans.push_back(matrix[i][left]);
+                }
+            left++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r);
+
+        for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
