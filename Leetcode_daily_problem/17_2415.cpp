479f5507c75eeabe14c26b3aea525cbf16b1751d
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-20 21:20:02 +0530

    using bfs, level travarsal

--- Leetcode_daily_problem/17_2415.cpp
+++ Leetcode_daily_problem/17_2415.cpp
@@ -10,3 +10,49 @@ The level of a node is the number of edges along the path between it and the roo
 
  
  */
+
+// ------------ BFS
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        if (!root) {
+            return nullptr;  // Return null if the tree is empty.
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);  // Start BFS with the root node.
+        int level = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<TreeNode*> currentLevelNodes;
+
+            // Process all nodes at the current level.
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                currentLevelNodes.push_back(node);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            // Reverse node values if the current level is odd.
+            if (level % 2 == 1) {
+                int left = 0, right = currentLevelNodes.size() - 1;
+                while (left < right) {
+                    int temp = currentLevelNodes[left]->val;
+                    currentLevelNodes[left]->val =
+                        currentLevelNodes[right]->val;
+                    currentLevelNodes[right]->val = temp;
+                    left++;
+                    right--;
+                }
+            }
+
+            level++;
+        }
+
+        return root;  // Return the modified tree root.
+    }
+};
