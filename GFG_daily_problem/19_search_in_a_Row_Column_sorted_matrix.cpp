26b703bc2b1e96b614d479bac4e7c26a73a44374
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-22 18:56:03 +0530

    search in a row column sorted amtrix

---
+++ GFG_daily_problem/19_search_in_a_Row_Column_sorted_matrix.cpp
@@ -0,0 +1,4 @@
+/**
+ * Given a 2D integer matrix mat[][] of size n x m, where every row and column is sorted in increasing order and a number x, 
+ * the task is to find whether element x is present in the matrix.
+ */
