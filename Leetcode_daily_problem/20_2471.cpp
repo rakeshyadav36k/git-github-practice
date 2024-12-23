4b5a0ee63a6831da31f2f659b3a85733b4e2ef76
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-23 22:28:45 +0530

    using hashmap

--- Leetcode_daily_problem/20_2471.cpp
+++ Leetcode_daily_problem/20_2471.cpp
@@ -7,3 +7,59 @@ Return the minimum number of operations needed to make the values at each level 
 
 The level of a node is the number of edges along the path between it and the root node.
  */
+
+class Solution {
+public:
+    int minimumOperations(TreeNode* root) {
+        queue<TreeNode*> queue;
+        queue.push(root);
+        int totalSwaps = 0;
+
+        // Process tree level by level using BFS
+        while (!queue.empty()) {
+            int levelSize = queue.size();
+            vector<int> levelValues(levelSize);
+
+            // Store values of current level and add children to queue
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = queue.front();
+                queue.pop();
+                levelValues[i] = node->val;
+
+                if (node->left != nullptr) queue.push(node->left);
+                if (node->right != nullptr) queue.push(node->right);
+            }
+
+            // Add minimum swaps needed for current level
+            totalSwaps += getMinSwaps(levelValues);
+        }
+        return totalSwaps;
+    }
+
+private:
+    // Calculate minimum swaps needed to sort an array
+    int getMinSwaps(vector<int>& original) {
+        int swaps = 0;
+        vector<int> target = original;
+        sort(target.begin(), target.end());
+
+        // Map to track current positions of values
+        unordered_map<int, int> pos;
+        for (int i = 0; i < original.size(); i++) {
+            pos[original[i]] = i;
+        }
+
+        // For each position, swap until correct value is placed
+        for (int i = 0; i < original.size(); i++) {
+            if (original[i] != target[i]) {
+                swaps++;
+
+                // Update position of swapped values
+                int curPos = pos[target[i]];
+                pos[original[i]] = curPos;
+                swap(original[curPos], original[i]);
+            }
+        }
+        return swaps;
+    }
+};
