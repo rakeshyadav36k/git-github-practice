8636030d10adad88a24f09c90969547f3b089fea
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-25 16:43:45 +0530

    using BFS

--- Leetcode_daily_problem/22_Find_largest_value_in_each_Tree_row.cpp
+++ Leetcode_daily_problem/22_Find_largest_value_in_each_Tree_row.cpp
@@ -3,3 +3,40 @@
  * Given the root of a binary tree, 
  * return an array of the largest value in each row of the tree (0-indexed).
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+ // ------------------using BFS
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root) return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> ans;
+        while(!q.empty()){
+            int temp = q.front()->val;
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                auto node = q.front();
+                q.pop();
+                temp = max(temp, node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
