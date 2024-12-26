37e1eb70798c0d267f57a6b890d33b6cc65c416c
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-26 19:00:03 +0530

    two sum

---
+++ GFG_daily_problem/23_two_sum.cpp
@@ -0,0 +1,4 @@
+/**
+ * Given an array arr[] of positive integers and another integer target. 
+ * Determine if there exists two distinct indices such that the sum of there elements is equals to target.
+ */
