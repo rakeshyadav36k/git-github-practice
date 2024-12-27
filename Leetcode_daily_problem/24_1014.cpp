573cda0e988d4862ea4bb97a4434d740bd07feda
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-27 20:39:24 +0530

    code

--- Leetcode_daily_problem/24_1014.cpp
+++ Leetcode_daily_problem/24_1014.cpp
@@ -6,3 +6,18 @@ The score of a pair (i < j) of sightseeing spots is values[i] + values[j] + i - 
 
 Return the maximum score of a pair of sightseeing spots.
  */
+
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int n = values.size();
+        int prevMaxSum = values[0] + 0;  // values[i] + i
+        int res = 0;
+        for(int i = 1; i < n; i++){
+            res = max(res, prevMaxSum + values[i] - i);  // prevMaxSum + values[j] - j
+            prevMaxSum = max(prevMaxSum, values[i] + i);
+        }
+
+        return res;
+    }
+};
