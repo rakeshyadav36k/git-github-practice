5afdc424e7058ddb203b07084d98d1852dd4469e
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-12-31 16:24:37 +0530

    dynamic programming

--- Leetcode_daily_problem/26_minimum_costs_for_tickets.cpp
+++ Leetcode_daily_problem/26_minimum_costs_for_tickets.cpp
@@ -11,3 +11,45 @@ The passes allow that many days of consecutive travel.
 For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
 Return the minimum number of dollars you need to travel every day in the given list of days.
  */
+
+class Solution {
+public:
+    unordered_set<int> isTravelNeeded;
+    
+    int solve(vector<int>& dp, vector<int>& days, vector<int>& costs, int currDay) {
+        // If we have iterated over travel days, return 0.
+        if (currDay > days[days.size() - 1]) {
+            return 0;
+        }
+        
+        // If we don't need to travel on this day, move on to next day.
+        if (isTravelNeeded.find(currDay) == isTravelNeeded.end()) {
+            return solve(dp, days, costs, currDay + 1);
+        }
+        
+        // If already calculated, return from here with the stored answer.
+        if (dp[currDay] != -1) {
+            return dp[currDay];
+        }
+        
+        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
+        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
+        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
+        
+        // Store the cost with the minimum of the three options.
+        return dp[currDay] = min(oneDay, min(sevenDay, thirtyDay));
+    }
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // The last day on which we need to travel.
+        int lastDay = days[days.size() - 1];
+        vector<int> dp(lastDay + 1, -1);
+        
+        // Mark the days on which we need to travel.
+        for (int day : days) {
+            isTravelNeeded.insert(day);
+        }
+        
+        return solve(dp, days, costs, 1);
+    }
+};
