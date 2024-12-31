1d640efb0b643cf389e9d38edee21b0b885dfa24
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-01 04:55:00 +0530

    using map

--- GFG_daily_problem/27_print_anagram_together.cpp
+++ GFG_daily_problem/27_print_anagram_together.cpp
@@ -3,3 +3,73 @@
 
 Note: The final output will be in lexicographic order.
  */
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<string>> anagrams(vector<string>& arr) {
+        vector<vector<string> > result;
+
+        // hash map to maintain groups of anagrams
+        unordered_map<string, vector<string> > umap;
+
+        for (int i = 0; i < arr.size(); i++) {
+            string s = arr[i];
+
+            // sort each string
+            sort(s.begin(), s.end());
+
+            // add original string to corresponding sorted string in hash map
+            umap[s].push_back(arr[i]);
+        }
+        for (auto itr = umap.begin(); itr != umap.end(); itr++)
+            result.push_back(itr->second);
+
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<string> arr;
+        string input_line;
+        getline(cin, input_line);
+        stringstream ss(input_line);
+        string word;
+        while (ss >> word) {
+            arr.push_back(word);
+        }
+
+        Solution ob;
+        vector<vector<string> > result = ob.anagrams(arr);
+        sort(result.begin(), result.end());
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = 0; j < result[i].size(); j++) {
+                cout << result[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
