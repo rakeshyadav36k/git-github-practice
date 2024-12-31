f16e176d3a9e74e404016aaec09b3be820af45d2
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-01 04:53:00 +0530

    code

--- Leetcode_daily_problem/27_3403.cpp
+++ Leetcode_daily_problem/27_3403.cpp
@@ -10,3 +10,21 @@ Find the lexicographically largest string from the box after all the rounds are 
 A string a is lexicographically smaller than a string b if in the first position where a and b differ, string a has a letter that appears earlier in the alphabet than the corresponding letter in b.
 If the first min(a.length, b.length) characters do not differ, then the shorter string is the lexicographically smaller one.
  */
+
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        int n = word.size();
+        int largestString = n - (numFriends - 1); // distribute (numFriends - 1) friends with single string and 
+                                                //remaining 1 student will have the largest string;
+        string res = "";
+        for(int i = 0; i < n ; i++){
+            string s = word.substr(i, largestString);
+            res = max(res, s);
+        }
+        return res;
+    }
+};
