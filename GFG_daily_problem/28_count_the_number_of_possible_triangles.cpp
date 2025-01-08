992f24b7f0d16936d0a97dc5dd2a0c33b9fcf895
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-08 19:34:28 +0530

    code

--- GFG_daily_problem/28_count_the_number_of_possible_triangles.cpp
+++ GFG_daily_problem/28_count_the_number_of_possible_triangles.cpp
@@ -4,3 +4,37 @@
 
 A triangle with three given sides is only possible if sum of any two sides is always greater than the third side.
  */
+
+class Solution {
+  public:
+    // Function to count the number of possible triangles.
+    int countTriangles(vector<int>& arr) {
+        int n = arr.size();
+        int count = 0;
+
+        sort(arr.begin(), arr.end());
+
+        for (int k = 2; k < n; k++)
+        {
+            int i = 0, j = k - 1;
+            
+           
+            while (i < j) 
+            {
+                if (arr[i] + arr[j] > arr[k]) 
+                {
+                   
+                    count += (j - i);
+                      j--; 
+                    
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
