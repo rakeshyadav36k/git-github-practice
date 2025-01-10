8318cb870806bbfe9447778095e5a9e4c13a0137
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-10 22:34:01 +0530

    using map

--- Leetcode_daily_problem/29_916_word_subsets.cpp
+++ Leetcode_daily_problem/29_916_word_subsets.cpp
@@ -8,3 +8,42 @@ A string a from words1 is universal if for every string b in words2, b is a subs
 
 Return an array of all the universal strings in words1. You may return the answer in any order
  */
+
+class Solution {
+public:
+    vector<int> count(string s){
+        vector<int> freq(26, 0);
+        for(char ch : s){
+            freq[ch - 'a']++;
+        }
+
+        return freq;
+    }
+
+    vector<string> wordSubsets(vector<string>& A, vector<string>& B) {
+        vector<int> bMax(26, 0);
+        for(auto b : B){
+            vector<int> bCount = count(b);
+            for(int i = 0; i < 26; i++){
+                bMax[i] = max(bMax[i], bCount[i]);  
+            }
+        }
+
+        vector<string> ans; 
+        for(string a : A){
+            bool isContinue = false;
+            vector<int> aCount = count(a);
+            for(int i = 0; i < 26; i++){
+                if(aCount[i] < bMax[i]){
+                    isContinue = true;
+                    continue;
+                }
+            }
+            if(isContinue) continue;
+
+            ans.push_back(a);
+        }
+
+        return ans;
+    }
+};
