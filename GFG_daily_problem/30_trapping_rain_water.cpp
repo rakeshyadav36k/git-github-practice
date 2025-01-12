4ca30703b70d9a3911924d88a2ac28ed0d983abc
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-12 23:27:51 +0530

    using space memory

--- GFG_daily_problem/30_trapping_rain_water.cpp
+++ GFG_daily_problem/30_trapping_rain_water.cpp
@@ -3,3 +3,55 @@
  * If the width of each block is 1, 
  * compute how much water can be trapped between the blocks during the rainy season. 
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        int n=arr.size();
+        vector<int>leftToright(n,0);
+        vector<int>rightToleft(n,0);
+        int max_val=INT_MIN;
+        for(int i=0;i<n;i++){
+            max_val=max(max_val,arr[i]);
+            leftToright[i]=max_val;
+        }
+        max_val=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            max_val=max(max_val,arr[i]);
+            rightToleft[i]=max_val;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=(min(leftToright[i],rightToleft[i]))-arr[i];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.maxWater(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
