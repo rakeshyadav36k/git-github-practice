92f418d55afc936673b75991a919932164060470
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-12 23:32:49 +0530

    using stack

--- Leetcode_daily_problem/30_2116.cpp
+++ Leetcode_daily_problem/30_2116.cpp
@@ -10,3 +10,46 @@ If locked[i] is '1', you cannot change s[i].
 But if locked[i] is '0', you can change s[i] to either '(' or ')'.
 Return true if you can make s a valid parentheses string. Otherwise, return false.
  */
+
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int length = s.size();
+        // If length of string is odd, return false.
+        if (length % 2 == 1) {
+            return false;
+        }
+
+        stack<int> openBrackets, unlocked;
+
+        // Iterate through the string to handle '(' and ')'
+        for (int i = 0; i < length; i++) {
+            if (locked[i] == '0') {
+                unlocked.push(i);
+            } else if (s[i] == '(') {
+                openBrackets.push(i);
+            } else if (s[i] == ')') {
+                if (!openBrackets.empty()) {
+                    openBrackets.pop();
+                } else if (!unlocked.empty()) {
+                    unlocked.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        // Match remaining open brackets with unlocked characters
+        while (!openBrackets.empty() && !unlocked.empty() &&
+               openBrackets.top() < unlocked.top()) {
+            openBrackets.pop();
+            unlocked.pop();
+        }
+
+        if (!openBrackets.empty()) {
+            return false;
+        }
+
+        return true;
+    }
+};
