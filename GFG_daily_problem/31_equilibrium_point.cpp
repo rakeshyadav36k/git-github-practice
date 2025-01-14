1fbfc04d91d1855bcdfe080fac7e0454237de5b5
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-14 21:22:18 +0530

    code

--- GFG_daily_problem/31_equilibrium_point.cpp
+++ GFG_daily_problem/31_equilibrium_point.cpp
@@ -5,3 +5,49 @@ The equilibrium point in an array is an index (0-based indexing) such that
 the sum of all elements before that index is the same as the sum of elements after it. 
 Return -1 if no such point exists. 
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    // Function to find equilibrium point in the array.
+    int findEquilibrium(vector<int> &arr) {
+        int n = arr.size();
+        int totalSum = accumulate(arr.begin(), arr.end(), 0);
+        
+        int leftSum = 0;
+        for(int i = 0; i < n; i++){
+            int rightSum = totalSum - leftSum - arr[i];
+            
+            if(leftSum == rightSum){
+                return i;
+            }
+            
+            leftSum += arr[i];
+        }
+        return -1;
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findEquilibrium(arr) << endl;
+        cout << "~" << endl;
+    }
+}
