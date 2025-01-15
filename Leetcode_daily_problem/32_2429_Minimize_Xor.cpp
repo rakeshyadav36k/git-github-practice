9a27c6c98ad7427b65714dfb1229c13d474858f2
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-15 22:31:31 +0530

    bitwise operations

--- Leetcode_daily_problem/32_2429_Minimize_Xor.cpp
+++ Leetcode_daily_problem/32_2429_Minimize_Xor.cpp
@@ -9,3 +9,51 @@ Return the integer x. The test cases are generated such that x is uniquely deter
 
 The number of set bits of an integer is the number of 1's in its binary representation.
  */
+
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        // Initialize result to num1. We will modify result.
+        int result = num1;
+
+        int targetSetBitsCount = __builtin_popcount(num2);
+        int setBitsCount = __builtin_popcount(result);
+
+        // Start with the least significant bit (bit 0).
+        int currentBit = 0;
+
+        // Add bits to result if it has fewer set bits than the target.
+        while (setBitsCount < targetSetBitsCount) {
+            // If the current bit in result is not set (0), set it to 1.
+            if (!isSet(result, currentBit)) {
+                setBit(result, currentBit);
+                setBitsCount++;
+            }
+            // Move to the next bit.
+            currentBit++;
+        }
+
+        // Remove bits from result if it has more set bits than the target.
+        while (setBitsCount > targetSetBitsCount) {
+            // If the current bit in result is set (1), unset it (make it 0).
+            if (isSet(result, currentBit)) {
+                unsetBit(result, currentBit);
+                setBitsCount--;
+            }
+            // Move to the next bit.
+            currentBit++;
+        }
+
+        return result;
+    }
+
+private:
+    // Helper function to check if the given bit position in x is set (1).
+    bool isSet(int x, int bit) { return x & (1 << bit); }
+
+    // Helper function to set the given bit position in x to 1.
+    void setBit(int &x, int bit) { x |= (1 << bit); }
+
+    // Helper function to unset the given bit position in x (set it to 0).
+    void unsetBit(int &x, int bit) { x &= ~(1 << bit); }
+};
