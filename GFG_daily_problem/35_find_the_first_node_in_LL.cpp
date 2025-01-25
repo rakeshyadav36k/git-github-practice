02f19735a989740ba1d6c9928a3444d5c23ff048
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-25 16:54:09 +0530

    lonked list

--- GFG_daily_problem/35_find_the_first_node_in_LL.cpp
+++ GFG_daily_problem/35_find_the_first_node_in_LL.cpp
@@ -6,3 +6,93 @@ Custom Input format:
 A head of a singly linked list and a pos (1-based index) which denotes the position of the node to which the last node points to. 
 If pos = 0, it means the last node points to null, indicating there is no loop.
  */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void loopHere(Node *head, Node *tail, int position) {
+    if (position == 0)
+        return;
+
+    Node *walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+/*struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution {
+  public:
+    Node* findFirstNode(Node* head) {
+        unordered_set<Node*> st;
+        while(head){
+            if(st.find(head) != st.end()) return head;
+            st.insert(head);
+            head = head->next;
+        }
+        return NULL;
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        assert(k <= arr.size());
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        loopHere(head, tail, k);
+
+        Solution ob;
+        Node *ans = ob.findFirstNode(head);
+        cout << (ans == NULL ? -1 : ans->data) << "\n";
+        cout << "~\n";
+    }
+    return 0;
+}
