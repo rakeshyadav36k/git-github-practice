45bcf96c298a95f3529debe3e4382e7e554d76e8
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-26 23:17:42 +0530

    remove loop in linked list

---
+++ GFG_daily_problem/36_remove_loop_in_LL.cpp
@@ -0,0 +1,152 @@
+/**
+ * Given the head of a linked list that may contain a loop.  
+ * A loop means that the last node of the linked list is connected back to a node in the same list. 
+ * The task is to remove the loop from the linked list (if it exists).
+
+Custom Input format:
+
+A head of a singly linked list and a pos (1-based index) which denotes the position of the node to which the last node points to. 
+If pos = 0, it means the last node points to null, indicating there is no loop.
+
+The generated output will be true if there is no loop in list and other nodes in the list remain unchanged, otherwise, false.
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position) {
+    if (position == 0)
+        return;
+
+    Node* walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head) {
+    if (!head)
+        return false;
+
+    Node* fast = head->next;
+    Node* slow = head;
+
+    while (fast != slow) {
+        if (!fast || !fast->next)
+            return false;
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return true;
+}
+
+int length(Node* head) {
+    int ret = 0;
+    while (head) {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+bool notOriginal(Node* head, unordered_map<Node*, int>& myMap) {
+
+    while (head) {
+        if (myMap.find(head) == myMap.end())
+            return true;
+        if (myMap[head] != (head->data))
+            return true;
+
+        head = head->next;
+    }
+}
+
+
+class Solution {
+  public:
+    // Function to remove a loop in the linked list.
+    void removeLoop(Node* head) {
+        Node* slow=head;
+        Node* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                break;
+            }
+        }
+        if(fast==NULL|| fast->next==NULL) return;
+        slow=head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        Node* nxtNode=slow; 
+        while(fast->next!=nxtNode){
+            fast=fast->next;
+        }
+        
+        fast->next=NULL;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        unordered_map<Node*, int> myMap;
+
+        int n, num;
+        n = arr.size();
+
+        Node *head, *tail;
+        num = arr[0];
+        head = tail = new Node(num);
+
+        myMap[head] = num;
+
+        for (int i = 1; i < n; i++) {
+            num = arr[i];
+            tail->next = new Node(num);
+            tail = tail->next;
+            myMap[tail] = num;
+        }
+
+        int pos;
+        cin >> pos;
+        cin.ignore();
+        loopHere(head, tail, pos);
+
+        Solution ob;
+        ob.removeLoop(head);
+
+        if (isLoop(head) || length(head) != n || notOriginal(head, myMap))
+            cout << "false\n";
+        else
+            cout << "true\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
