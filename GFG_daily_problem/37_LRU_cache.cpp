c5f7505b8f2f7331afaba64fdca38b96150483a4
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-27 21:53:06 +0530

    using doubly LL

--- GFG_daily_problem/37_LRU_cache.cpp
+++ GFG_daily_problem/37_LRU_cache.cpp
@@ -9,3 +9,74 @@ get(key): returns the value of the key if it already exists in the cache otherwi
 put(key, value): if the key is already present, update its value. If not present, add the key-value pair to the cache. If the cache reaches its capacity it should remove the least recently used item before inserting the new item.
 In the constructor of the class the capacity of the cache should be initialized.
 */
+
+class LRUCache {
+  private:
+    class Node{
+    public:
+        int key;
+        int val;
+        Node *prev;
+        Node *next;
+        Node(int _key, int _val){
+            key = _key;
+            val = _val;
+        }
+    };
+    Node *head = new Node(-1, -1);
+    Node *tail = new Node(-1, -1);
+    int cap;
+    unordered_map<int,Node*> mp;
+  public:
+    // Constructor for initializing the cache capacity with the given value.
+    LRUCache(int capacity) {
+        cap = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void addNode(Node *newNode){
+        Node *temp = head->next;
+        newNode->next = temp;
+        newNode->prev = head;
+        head->next = newNode;
+        temp->prev = newNode;
+    }
+
+    void deleteNode(Node *delNode){
+        Node *delPrev = delNode->prev;
+        Node *delNext = delNode->next;
+        delPrev->next = delNext;
+        delNext->prev = delPrev;
+    }
+
+    // Function to return value corresponding to the key.
+    int get(int key_) {
+        if(mp.find(key_) != mp.end()){
+            Node *resNode = mp[key_];
+            int res = resNode->val;
+            mp.erase(key_);
+            deleteNode(resNode);
+            addNode(resNode);
+            mp[key_] = head->next;
+            return res;
+        }
+        return -1;
+    }
+
+    // Function for storing key-value pair.
+    void put(int key_, int value) {
+        if(mp.find(key_) != mp.end()){
+            Node *existingNode = mp[key_];
+            mp.erase(key_);
+            deleteNode(existingNode);
+        }
+        if(mp.size() == cap){
+            mp.erase(tail->prev->key);
+            deleteNode(tail->prev);
+        }
+
+        addNode(new Node(key_, value));
+        mp[key_] = head->next;
+    }
+};
