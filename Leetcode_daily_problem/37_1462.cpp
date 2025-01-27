a40427c1155b4e5abfea4c223501d28c39575e92
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-27 21:50:40 +0530

    topological sort, kahn algo

--- Leetcode_daily_problem/37_1462.cpp
+++ Leetcode_daily_problem/37_1462.cpp
@@ -8,3 +8,51 @@ You are also given an array queries where queries[j] = [uj, vj]. For the jth que
 
 Return a boolean array answer, where answer[j] is the answer to the jth query.
  */
+
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses,
+                                     vector<vector<int>>& prerequisites,
+                                     vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adjList;
+        vector<int> indegree(numCourses, 0);
+        for (auto edge : prerequisites) {
+            adjList[edge[0]].push_back(edge[1]);
+            indegree[edge[1]]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (!indegree[i]) {
+                q.push(i);
+            }
+        }
+
+        // Map from the node as key to the set of prerequisite nodes.
+        unordered_map<int, unordered_set<int>> nodePrerequisites;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto adj : adjList[node]) {
+                // Add node and prerequisite of node to the prerequisites of adj
+                nodePrerequisites[adj].insert(node);
+                for (auto prereq : nodePrerequisites[node]) {
+                    nodePrerequisites[adj].insert(prereq);
+                }
+
+                indegree[adj]--;
+                if (!indegree[adj]) {
+                    q.push(adj);
+                }
+            }
+        }
+
+        vector<bool> answer;
+        for (auto q : queries) {
+            answer.push_back(nodePrerequisites[q[1]].contains(q[0]));
+        }
+
+        return answer;
+    }
+};
