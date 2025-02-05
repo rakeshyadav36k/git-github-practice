b2349d39113e5ef184e0b528882539ec7fe6a79d
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-02-05 23:01:33 +0530

    iterative method

--- Leetcode_daily_problem/41_1790.cpp
+++ Leetcode_daily_problem/41_1790.cpp
@@ -4,3 +4,24 @@
 
 Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
  */
+
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.length();
+        int i;
+        for(i = 0; i < n; i++){
+            if(s1[i] != s2[i]) break;
+        }
+
+        // we want to make s2 equal to s1 by swapping opn in s2
+        for(int j = 0; j < n; j++){
+            string temp = s2;
+            swap(s2[i], s2[j]);
+            if(s1 == s2) return true;
+            s2 = temp;
+        }
+
+        return i == n ? true : false;
+    }
+};
