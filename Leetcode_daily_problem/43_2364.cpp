f83997825e081d653165a790bdbac7176afdd471
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-02-09 23:04:36 +0530

    count number of bad pairs

---
+++ Leetcode_daily_problem/43_2364.cpp
@@ -0,0 +1,6 @@
+/**
+ * You are given a 0-indexed integer array nums. 
+ * A pair of indices (i, j) is a bad pair if i < j and j - i != nums[j] - nums[i].
+
+Return the total number of bad pairs in nums.
+ */
