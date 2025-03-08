f92b29f2e12e14c586c09f752a4bc8e0c1348b82
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-03-08 23:40:38 +0530

    using queue

--- Leetcode_daily_problem/45_2379.cpp
+++ Leetcode_daily_problem/45_2379.cpp
@@ -7,3 +7,37 @@ In one operation, you can recolor a white block such that it becomes a black blo
 
 Return the minimum number of operations needed such that there is at least one occurrence of k consecutive black blocks.
  */
+
+// 1st method - using queue
+class Solution {
+    public:
+        int minimumRecolors(string blocks, int k) {
+            queue<char> blockQueue;
+            int numWhites = 0;
+    
+            // Initiate queue with first k values
+            while (blockQueue.size() < k) {
+                char currentChar = blocks[blockQueue.size()];
+                if (currentChar == 'W') numWhites++;
+                blockQueue.push(currentChar);
+            }
+    
+            // Set initial minimum
+            int numRecolors = numWhites;
+            for (int i = k; i < blocks.size(); i++) {
+                // Remove top element from queue and update current number of white
+                // blocks
+                if (blockQueue.front() == 'W') numWhites--;
+                blockQueue.pop();
+    
+                // Add current element to queue and update current number of white
+                // blocks
+                if (blocks[i] == 'W') numWhites++;
+                blockQueue.push(blocks[i]);
+    
+                // Update minimum
+                numRecolors = min(numRecolors, numWhites);
+            }
+            return numRecolors;
+        }
+    };
