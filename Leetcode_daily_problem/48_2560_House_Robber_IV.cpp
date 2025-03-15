03f3686d7af82c423ebdf2ea4ff3c86c63274cb2
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-03-15 23:03:37 +0530

    binary search

--- Leetcode_daily_problem/48_2560_House_Robber_IV.cpp
+++ Leetcode_daily_problem/48_2560_House_Robber_IV.cpp
@@ -9,3 +9,38 @@ You are also given an integer k, representing the minimum number of houses the r
 
 Return the minimum capability of the robber out of all the possible ways to steal at least k houses.
  */
+
+class Solution
+{
+public:
+    int minCapability(vector<int> &nums, int k)
+    {
+        // Store the maximum nums value in maxReward.
+        int minReward = 1, maxReward = *max_element(nums.begin(), nums.end()),
+            totalHouses = nums.size();
+
+        // Use binary search to find the minimum reward possible.
+        while (minReward < maxReward)
+        {
+            int midReward = (minReward + maxReward) / 2;
+            int possibleThefts = 0;
+
+            for (int index = 0; index < totalHouses; ++index)
+            {
+                if (nums[index] <= midReward)
+                {
+                    possibleThefts += 1;
+                    index++; // Skip the next house to maintain the
+                             // non-adjacent condition
+                }
+            }
+
+            if (possibleThefts >= k)
+                maxReward = midReward;
+            else
+                minReward = midReward + 1;
+        }
+
+        return minReward;
+    }
+};
