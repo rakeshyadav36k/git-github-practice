3b8e0c1e9db6c2cae79a1fa5b1ac30fc145a13e3
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-03-20 22:25:58 +0530

    dp and bottom-up approach

--- GFG_daily_problem/48_Stock_Buy_and_Sell.cpp
+++ GFG_daily_problem/48_Stock_Buy_and_Sell.cpp
@@ -5,3 +5,63 @@
 You are given an array prices[] representing stock prices throughout the day. 
 Note that the second transaction can only start after the first one is complete (buy->sell->buy->sell).
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        int n=prices.size();
+            vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int> (3,0)));
+            
+            for(int index=n-1;index>=0;index--)
+            {
+                for(int buy=0;buy<=1;buy++)
+                {
+                    for(int limit=1;limit<=2;limit++)
+                    {
+                        if(buy==1)
+                        {
+                            int buyit= -prices[index]+dp[index+1][0][limit];
+                            int skip= 0+ dp[index+1][1][limit];
+                            dp[index][buy][limit]=max(buyit,skip);
+                        }
+                        else
+                        {
+                            int sell=prices[index]+dp[index+1][1][limit-1];
+                            int skip=0+dp[index+1][0][limit];
+                            dp[index][buy][limit]=max(sell,skip);
+                        }
+                    }
+                }
+            }
+        
+        return dp[0][1][2];
+        
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.maxProfit(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
