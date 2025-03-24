730d485e88b51b6055d88794d3a7104106a609de
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-03-24 23:02:18 +0530

    Count ways without meetings

---
+++ Leetcode_daily_problem/52_3169.cpp
@@ -0,0 +1,40 @@
+/**
+ * You are given a positive integer days representing the total number of days an employee is available for work (starting from day 1). You are also given a 2D array meetings of size n where, meetings[i] = [start_i, end_i] represents the starting and ending days of meeting i (inclusive).
+
+Return the count of days when the employee is available for work but no meetings are scheduled.
+
+Note: The meetings may overlap.
+ */
+class Solution {
+    public:
+        int countDays(int days, vector<vector<int>>& meetings) {
+            map<int, int> dayMap;
+            int prefixSum = 0, freeDays = 0, previousDay = days;
+            bool hasGap = false;
+    
+            for (auto& meeting : meetings) {
+                // Set first day of meetings
+                previousDay = min(previousDay, meeting[0]);
+    
+                // Process start and end of meeting
+                dayMap[meeting[0]]++;
+                dayMap[meeting[1] + 1]--;
+            }
+    
+            // Add all days before the first day of meetings
+            freeDays += (previousDay - 1);
+            for (auto& day : dayMap) {
+                int currentDay = day.first;
+                // Add current range of days without a meeting
+                if (prefixSum == 0) {
+                    freeDays += currentDay - previousDay;
+                }
+                prefixSum += day.second;
+                previousDay = currentDay;
+            }
+    
+            // Add all days after the last day of meetings
+            freeDays += days - previousDay + 1;
+            return freeDays;
+        }
+    };
