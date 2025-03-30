f17988f498004111ac714f872ebf9492e074c937
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-03-30 23:41:07 +0530

    greedy algo

--- GFG_daily_problem/49_gas_station.cpp
+++ GFG_daily_problem/49_gas_station.cpp
@@ -4,3 +4,57 @@
  * You have a car with an unlimited gas tank. You begin the journey with an empty tank from one of the gas stations. Return the index of the starting gas station if it's possible to travel around the circuit without running out of gas at any station in a clockwise direction. If there is no such starting station exists, return -1.
 Note: If a solution exists, it is guaranteed to be unique.
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        int negSum = 0;
+        int sum = 0;
+        int start = -1;
+        for(int i=0; i<cost.size(); i++){
+            if(sum==0) start =i;
+            sum += (gas[i] - cost[i]);
+            if(sum < 0){
+                negSum += sum;
+                sum = 0;
+            }
+        }
+        return (sum + negSum <0) ? -1 : start;
+    }
+};
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+
+        vector<int> gas, cost;
+        string input1;
+        getline(cin, input1);
+        stringstream ss(input1);
+        int number1;
+        while (ss >> number1) {
+            gas.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream sss(input2);
+        int number2;
+        while (sss >> number2) {
+            cost.push_back(number2);
+        }
+        Solution ob;
+        int ans = ob.startStation(gas, cost);
+
+        cout << ans << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
