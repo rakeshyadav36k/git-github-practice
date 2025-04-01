c82a807f4b2e74857dc449c9c66a1c2f2dd4cb67
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-01 23:03:33 +0530

    recursive + dp

--- Leetcode_daily_problem/57_2140.cpp
+++ Leetcode_daily_problem/57_2140.cpp
@@ -8,6 +8,27 @@ If question 0 is solved, you will earn 3 points but you will be unable to solve 
 If instead, question 0 is skipped and question 1 is solved, you will earn 4 points but you will be unable to solve questions 2 and 3.
  */
 
+ // 1st- recursive + dp
+ long long dp[100000];
+class Solution {
+public:
+    long long dfs(vector<vector<int>>& q, int i){
+        if (i>=q.size()) return 0;
+        if (dp[i]!=-1) return dp[i];
+        long long pt=q[i][0];
+        int skip=q[i][1];
+        dp[i]=max(pt+dfs(q, i+skip+1), dfs(q, i+1));
+        return dp[i];
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        const int n=questions.size();
+        memset(dp, -1, n*sizeof(long long));
+        return dfs(questions, 0);       
+    }
+};
+
+
+ // bottom-up
  class Solution {
     public:
         long long mostPoints(vector<vector<int>>& questions) {
