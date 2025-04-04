b3ce4c9d85f972363b63df5a65badd89825adbf7
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-04 23:11:59 +0530

    recursion

--- Leetcode_daily_problem/59_1123.cpp
+++ Leetcode_daily_problem/59_1123.cpp
@@ -7,3 +7,25 @@ The node of a binary tree is a leaf if and only if it has no children
 The depth of the root of the tree is 0. if the depth of a node is d, the depth of each of its children is d + 1.
 The lowest common ancestor of a set S of nodes, is the node A with the largest depth such that every node in S is in the subtree with root A.
  */
+
+ class Solution {
+    public:
+        pair<TreeNode*, int> dfs(TreeNode* root) {
+            if (!root) {
+                return {nullptr, 0};
+            }
+    
+            auto left = dfs(root->left);
+            auto right = dfs(root->right);
+    
+            if (left.second > right.second) {
+                return {left.first, left.second + 1};
+            }
+            if (left.second < right.second) {
+                return {right.first, right.second + 1};
+            }
+            return {root, left.second + 1};
+        }
+    
+        TreeNode* lcaDeepestLeaves(TreeNode* root) { return dfs(root).first; }
+    };
