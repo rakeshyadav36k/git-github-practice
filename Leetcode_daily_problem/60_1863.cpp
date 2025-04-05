b4f985f00a93b1b54b19ed0ef049da4c74eaf7ce
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-05 23:00:22 +0530

    optimized backtracking

--- Leetcode_daily_problem/60_1863.cpp
+++ Leetcode_daily_problem/60_1863.cpp
@@ -48,3 +48,26 @@ An array a is a subset of an array b if a can be obtained from b by deleting som
             generateSubsets(nums, index + 1, subset, subsets);
         }
     };
+
+    // 2nd - Optimized Backtracking
+    class Solution {
+        public:
+            int subsetXORSum(vector<int>& nums) {
+                return XORSum(nums, 0, 0); 
+            }
+        
+        private:
+            int XORSum(vector<int>& nums, int index, int currentXOR) {
+                // Return currentXOR when all elements in nums are already considered
+                if (index == nums.size()) return currentXOR;
+        
+                // Calculate sum of subset xor with current element
+                int withElement = XORSum(nums, index + 1, currentXOR ^ nums[index]);
+        
+                // Calculate sum of subset xor without current element
+                int withoutElement = XORSum(nums, index + 1, currentXOR);
+        
+                // Return sum of xor totals
+                return withElement + withoutElement;
+            }
+        };
