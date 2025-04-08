3f988047f9a0cfc7412f8c095cda1b312e48261c
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-08 23:19:58 +0530

    simulation

--- Leetcode_daily_problem/62_3396.cpp
+++ Leetcode_daily_problem/62_3396.cpp
@@ -4,3 +4,28 @@
 Remove 3 elements from the beginning of the array. If the array has fewer than 3 elements, remove all remaining elements.
 Note that an empty array is considered to have distinct elements. Return the minimum number of operations needed to make the elements in the array distinct.
  */
+
+ // 1st- simulation
+ class Solution {
+    public:
+        int minimumOperations(vector<int>& nums) {
+            auto checkUnique = [&](int start) {
+                unordered_set<int> seen;
+                for (int i = start; i < nums.size(); i++) {
+                    if (seen.count(nums[i])) {
+                        return false;
+                    }
+                    seen.emplace(nums[i]);
+                }
+                return true;
+            };
+    
+            int ans = 0;
+            for (int i = 0; i < nums.size(); i += 3, ans++) {
+                if (checkUnique(i)) {
+                    return ans;
+                }
+            }
+            return ans;
+        }
+    };
