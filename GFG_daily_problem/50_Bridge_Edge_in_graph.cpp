5a0570252147063b650c49dea69608a0aac1e255
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-08 23:23:11 +0530

    dfs

--- GFG_daily_problem/50_Bridge_Edge_in_graph.cpp
+++ GFG_daily_problem/50_Bridge_Edge_in_graph.cpp
@@ -6,3 +6,28 @@ Note:
 An edge is called a bridge if removing it increases the number of connected components of the graph.
 if there’s only one path between c and d (which is the edge itself), then that edge is a bridge.
  */
+
+ class Solution {
+    public:
+      void dfs(int node,vector<int>adj[],int c, int d,vector<int>&vis){
+          vis[node]=true;
+          for(auto it:adj[node]){
+              if(!vis[it] && !(node==c && it==d)){
+                  dfs(it,adj,c,d,vis);
+              }
+          }
+      }
+      bool isBridge(int V, vector<vector<int>> &edges, int c, int d) {
+          vector<int>adj[V];
+          for(int i=0;i<edges.size();i++){
+              int u=edges[i][0];
+              int v=edges[i][1];
+              adj[u].push_back(v);
+              adj[v].push_back(u);
+          }
+          vector<int>vis(V,0);
+          dfs(c,adj,c,d,vis);
+          if(vis[d]) return false;
+          return true;
+      }
+  };
