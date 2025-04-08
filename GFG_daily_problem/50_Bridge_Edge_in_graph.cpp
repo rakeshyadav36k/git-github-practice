fcc98cf39ceaa7042ff6f7debca02e43e881331a
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-08 23:22:29 +0530

    bridge edge in a graph

---
+++ GFG_daily_problem/50_Bridge_Edge_in_graph.cpp
@@ -0,0 +1,8 @@
+/**
+ * Given an undirected graph with V vertices numbered from 0 to V-1 and E edges, represented by 2d array edges[][], where edges[i]=[u,v] represents the edge between the vertices u and v. Determine whether a specific edge between two vertices (c, d) is a bridge.
+
+Note:
+
+An edge is called a bridge if removing it increases the number of connected components of the graph.
+if there’s only one path between c and d (which is the edge itself), then that edge is a bridge.
+ */
