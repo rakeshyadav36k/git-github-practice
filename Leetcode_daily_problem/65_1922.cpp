4959ed3322d57502af785c0ef2b6d79ef40a7476
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-13 23:31:49 +0530

    fast exponentiation

--- Leetcode_daily_problem/65_1922.cpp
+++ Leetcode_daily_problem/65_1922.cpp
@@ -6,3 +6,26 @@ Given an integer n, return the total number of good digit strings of length n. S
 
 A digit string is a string consisting of digits 0 through 9 that may contain leading zeros.
  */
+
+ class Solution {
+    private:
+        static constexpr int mod = 1000000007;
+    
+    public:
+        int countGoodNumbers(long long n) {
+            // use fast exponentiation to calculate x^y % mod
+            auto quickmul = [](int x, long long y) -> int {
+                int ret = 1, mul = x;
+                while (y > 0) {
+                    if (y % 2 == 1) {
+                        ret = (long long)ret * mul % mod;
+                    }
+                    mul = (long long)mul * mul % mod;
+                    y /= 2;
+                }
+                return ret;
+            };
+    
+            return (long long)quickmul(5, (n + 1) / 2) * quickmul(4, n / 2) % mod;
+        }
+    };
