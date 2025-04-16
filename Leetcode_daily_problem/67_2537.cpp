bed28e9906e1e6e587736312ff91ef185db6a7ac
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-16 22:38:19 +0530

    two pointers

--- Leetcode_daily_problem/67_2537.cpp
+++ Leetcode_daily_problem/67_2537.cpp
@@ -5,3 +5,26 @@ A subarray arr is good if there are at least k pairs of indices (i, j) such that
 
 A subarray is a contiguous non-empty sequence of elements within an array.
  */
+
+ class Solution {
+    public:
+        long long countGood(vector<int>& nums, int k) {
+            int n = nums.size();
+            int same = 0, right = -1;
+            unordered_map<int, int> cnt;
+            long long ans = 0;
+            for (int left = 0; left < n; ++left) {
+                while (same < k && right + 1 < n) {
+                    ++right;
+                    same += cnt[nums[right]];
+                    ++cnt[nums[right]];
+                }
+                if (same >= k) {
+                    ans += n - right;
+                }
+                --cnt[nums[left]];
+                same -= cnt[nums[left]];
+            }
+            return ans;
+        }
+    };
