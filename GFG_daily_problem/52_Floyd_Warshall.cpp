0e2b9fecba41bf4dd2274c2435bfdfc21dbccdec
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-16 22:41:53 +0530

    Floyd warshall

---
+++ GFG_daily_problem/52_Floyd_Warshall.cpp
@@ -0,0 +1,49 @@
+/**
+ * You are given an weighted directed graph, represented by an adjacency matrix, dist[][] of size n x n, where dist[i][j] represents the weight of the edge from node i to node j. If there is no direct edge, dist[i][j] is set to a large value (i.e., 108) to represent infinity.
+The graph may contain negative edge weights, but it does not contain any negative weight cycles.
+
+Your task is to find the shortest distance between every pair of nodes i and j in the graph.
+
+Note: Modify the distances for every pair in place.
+ */
+
+class Solution
+{
+public:
+    void shortest_distance(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == -1)
+                    matrix[i][j] = 1e9;
+                if (i == j)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        for (int via = 0; via < n; via++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    matrix[i][j] = min(matrix[i][j], matrix[i][via] + matrix[via][j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 1e9)
+                    matrix[i][j] = -1;
+            }
+        }
+    }
+};
