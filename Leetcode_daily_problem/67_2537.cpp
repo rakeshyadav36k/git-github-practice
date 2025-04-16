967547505053750660b32081204004f516cc9386
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-16 22:37:52 +0530

    Count the nunber of good subarrays

---
+++ Leetcode_daily_problem/67_2537.cpp
@@ -0,0 +1,7 @@
+/**
+ * Given an integer array nums and an integer k, return the number of good subarrays of nums.
+
+A subarray arr is good if there are at least k pairs of indices (i, j) such that i < j and arr[i] == arr[j].
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+ */
