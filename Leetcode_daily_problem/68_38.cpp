d35061daacd72ecd586b4b7bffe3270efeb83ebc
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-18 14:42:55 +0530

    brute force

--- Leetcode_daily_problem/68_38.cpp
+++ Leetcode_daily_problem/68_38.cpp
@@ -7,3 +7,25 @@ Run-length encoding (RLE) is a string compression method that works by replacing
 
 Given a positive integer n, return the nth element of the count-and-say sequence.
  */
+// 1st - Brute force
+class Solution {
+    public:
+        string countAndSay(int n) {
+            string res = "1";
+            for (int i = 1; i < n; i++) {
+                string temp = "";
+                int count = 1;
+                for (int j = 1; j < res.size(); j++) {
+                    if (res[j] == res[j - 1]) {
+                        count++;
+                    } else {
+                        temp += to_string(count) + res[j - 1];
+                        count = 1;
+                    }
+                }
+                temp += to_string(count) + res.back();
+                res = temp;
+            }
+            return res;
+        }
+    };
