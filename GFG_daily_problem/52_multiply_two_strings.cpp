0377daf92f53ca6f1a36643a8914e62638875d03
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-27 21:14:29 +0530

    multiply two strings

---
+++ GFG_daily_problem/52_multiply_two_strings.cpp
@@ -0,0 +1,4 @@
+/**
+ * Given two numbers as strings s1 and s2. Calculate their Product.
+Note: The numbers can be negative and You are not allowed to use any built-in function or convert the strings to integers. There can be zeros in the begining of the numbers. You don't need to specify '+' sign in the begining of positive numbers.
+ */
