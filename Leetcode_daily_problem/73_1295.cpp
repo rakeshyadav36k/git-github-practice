3f5cdd513d92fc6523d17df7da093b99cf4b8a91
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-30 16:51:58 +0530

    Extract digits

--- Leetcode_daily_problem/73_1295.cpp
+++ Leetcode_daily_problem/73_1295.cpp
@@ -1,3 +1,29 @@
 /**
  * Given an array nums of integers, return how many of them contain an even number of digits.
  */
+
+ // 1st - Extract digits
+ class Solution {
+    public:
+        // Helper function to check if the number of digits is even
+        bool hasEvenDigits(int num) {
+            int digitCount = 0;
+            while (num) {
+                digitCount++;
+                num /= 10;
+            }
+            return (digitCount & 1) == 0;
+        }
+    
+        int findNumbers(vector<int>& nums) {
+            // Counter to count the number of even digit integers
+            int evenDigitCount = 0;
+    
+            for (int num : nums) {
+                if (hasEvenDigits(num))
+                    evenDigitCount++;
+            }
+    
+            return evenDigitCount;
+        }
+    };
