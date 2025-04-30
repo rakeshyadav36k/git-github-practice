0bb635100b9a2e49002dc9a268c0233634fc7c12
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-30 16:54:55 +0530

    constraint analysis

--- Leetcode_daily_problem/73_1295.cpp
+++ Leetcode_daily_problem/73_1295.cpp
@@ -63,3 +63,20 @@ class Solution {
             return evenDigitCount;
         }
     };
+
+
+// 4th - constraint analysis
+class Solution {
+    public:
+        int findNumbers(vector<int>& nums) {
+            // Counter to count the number of even digit integers
+            int evenDigitCount = 0;
+    
+            for (int num : nums) {
+                if ((num >= 10 && num <= 99) || (num >= 1000 && num <= 9999) || num == 100000)
+                    evenDigitCount++;
+            }
+    
+            return evenDigitCount;
+        }
+    };
