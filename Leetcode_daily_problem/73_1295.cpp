f28274f4ab41b93fa93edb67e4b4b7acdec172dd
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-04-30 16:53:59 +0530

    using logarithm

--- Leetcode_daily_problem/73_1295.cpp
+++ Leetcode_daily_problem/73_1295.cpp
@@ -45,3 +45,21 @@ class Solution {
             return evenDigitCount;
         }
     };
+
+// 3rd - using logarithm
+class Solution {
+    public:
+        int findNumbers(vector<int>& nums) {
+            // Counter to count the number of even digit integers
+            int evenDigitCount = 0;
+    
+            for (int num : nums) {
+                // Compute the number of digits in the num
+                int digitCount = (int) floor(log10(num)) + 1;
+                if (digitCount % 2 == 0)
+                    evenDigitCount++;
+            }
+    
+            return evenDigitCount;
+        }
+    };
