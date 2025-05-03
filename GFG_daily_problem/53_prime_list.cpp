b8b5c471675f23fc38be917d4c3e93be00477fd7
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-05-03 23:41:26 +0530

    prime list

---
+++ GFG_daily_problem/53_prime_list.cpp
@@ -0,0 +1,6 @@
+/**
+ * You are given the head of a linked list. 
+ * You have to replace all the values of the nodes with the nearest prime number. 
+ * If more than one prime number exists at an equal distance, choose the smallest one.
+ *  Return the head of the modified linked list.
+ */
