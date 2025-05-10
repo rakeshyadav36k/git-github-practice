4007441321c1e16a1916e34fed3757787fc6ac0d
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-05-10 23:53:53 +0530

    using prefix sum

---
+++ GFG_daily_problem/55_Longest_Subaaray_with_Majority_Greater_Than_K.cpp
@@ -0,0 +1,39 @@
+/**
+ * Given an array arr[] and an integer k, 
+ * the task is to find the length of longest subarray 
+ * in which the count of elements greater than k is more than the count of elements less than or equal to k.
+ */
+
+ class Solution {
+  public:
+    int longestSubarray(vector<int> &arr, int k) {
+        int n=arr.size();
+        vector<int>diff(n);
+        for(int i=0;i<n;i++){
+            if(arr[i]>k){
+                diff[i]=1;
+            }
+            else{
+                diff[i]=-1;
+            }
+        }
+        unordered_map<int,int>mp;
+        int prefix=0;
+        int maxlen=0;
+        for(int i=0;i<n;i++){
+            prefix+=diff[i];
+            if(prefix>0){
+                maxlen=i+1;
+            }
+            else{
+                if(mp.find(prefix-1)!=mp.end()){
+                    maxlen=max(maxlen,i-mp[prefix-1]);
+                }
+            }
+            if(mp.find(prefix)==mp.end()){
+                mp[prefix]=i;
+            }
+        }
+        return maxlen;
+    }
+};
