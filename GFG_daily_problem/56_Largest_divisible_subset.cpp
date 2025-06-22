d216d3f4c6bbccff61f3a4e972812e13b28aa2b8
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-06-22 21:25:59 +0530

    Largest divisible subset

---
+++ GFG_daily_problem/56_Largest_divisible_subset.cpp
@@ -0,0 +1,4 @@
+/**
+ * Given an array arr[] of distinct positive integers. Your task is to find the largest subset such that for every pair of elements (x, y) in the subset, either x divides y or y divides x.
+Note : If multiple subsets of the same maximum length exist, return the one that is lexicographically greatest, after sorting the subset in ascending order.
+ */
---
+++ Leetcode_daily_problem/temp.js
@@ -0,0 +1,10 @@
+let name = "First line"
+console.log(`${name}`);
+
+setTimeout(()=>{
+    console.log("Middle line");    
+}, 1000)
+
+console.log("Last line");
+
+
