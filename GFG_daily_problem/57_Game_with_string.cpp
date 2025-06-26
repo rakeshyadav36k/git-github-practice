fa25816b883d971c594fcbd58ab60fa6bad3065e
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-06-26 23:01:10 +0530

    using hashing

--- GFG_daily_problem/57_Game_with_string.cpp
+++ GFG_daily_problem/57_Game_with_string.cpp
@@ -4,6 +4,8 @@
 The value of the string is defined as the sum of the squares of the frequencies of each distinct character present in the string.
  */
 
+// 1st- priority queue
+
  int minValue(string &s, int k) {
         // code here
         priority_queue<int> pq;
@@ -25,6 +27,23 @@ The value of the string is defined as the sum of the squares of the frequencies 
             int m=pq.top();
             ans+=m*m;
             pq.pop();
+        }
+        return ans;
+    }
+
+// 2nd- Using Hashing
+int minValue(string &s, int k){
+        int hash[26] = {0};
+        for(auto i : s){
+            hash[i - 'a']++;
+        }
+        sort(hash, hash+26);
+        
+        int ans = 0, rem = s.size()-k;
+        for(int i=0, n=26;i<26;i++,n--){
+            int mn = min(hash[i], rem/n);
+            ans += mn*mn;
+            rem -= mn;
         }
         return ans;
     }
