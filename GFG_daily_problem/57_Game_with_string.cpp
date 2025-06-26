8bae349fa4f250e95719e9fa387e089f9d4ad1bc
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-06-26 22:57:27 +0530

    game with string

---
+++ GFG_daily_problem/57_Game_with_string.cpp
@@ -0,0 +1,5 @@
+/**
+ * Given a string s consisting of lowercase alphabets and an integer k, your task is to find the minimum possible value of the string after removing exactly k characters.
+
+The value of the string is defined as the sum of the squares of the frequencies of each distinct character present in the string.
+ */
