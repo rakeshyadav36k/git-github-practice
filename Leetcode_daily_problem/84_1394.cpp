bd5a7090ac49cc29d0036a609f0a0fb817c721f2
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-07-05 20:09:08 +0530

    using map

--- Leetcode_daily_problem/84_1394.cpp
+++ Leetcode_daily_problem/84_1394.cpp
@@ -3,3 +3,24 @@
 
 Return the largest lucky integer in the array. If there is no lucky integer return -1.
  */
+
+ class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int, int> freq;
+
+        for (int num : arr) {
+            freq[num]++;
+        }
+
+        int lucky = -1;
+
+        for (auto& [key, value] : freq) {
+            if (key == value) {
+                lucky = max(lucky, key);
+            }
+        }
+
+        return lucky;
+    }
+};
