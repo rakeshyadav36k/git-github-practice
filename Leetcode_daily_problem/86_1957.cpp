0a09730391661bb2541cbcea692ea82927e2baa9
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-07-21 22:15:46 +0530

    iteration

--- Leetcode_daily_problem/86_1957.cpp
+++ Leetcode_daily_problem/86_1957.cpp
@@ -5,3 +5,17 @@ Given a string s, delete the minimum possible number of characters from s to mak
 
 Return the final string after the deletion. It can be shown that the answer will always be unique.
  */
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int n = s.length();
+        string res = "";
+        for(int i = 0; i < n; i++){
+            if(i >= 2 && (s[i-1] == s[i] && s[i-2] == s[i])){
+                continue;
+            }
+            else res += s[i];
+        }
+        return res;
+    }
+};
