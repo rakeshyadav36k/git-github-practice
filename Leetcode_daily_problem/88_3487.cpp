cfe613bf74ea248df6c36841c883442f5d92e1c0
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-07-25 23:44:00 +0530

    using set

--- Leetcode_daily_problem/88_3487.cpp
+++ Leetcode_daily_problem/88_3487.cpp
@@ -7,3 +7,20 @@ All elements in the subarray are unique.
 The sum of the elements in the subarray is maximized.
 Return the maximum sum of such a subarray.
  */
+int maximumUniqueSubarray(vector<int>& nums) {
+    unordered_set<int> seen;
+    int max_sum = 0, current_sum = 0, left = 0;
+
+    for (int right = 0; right < nums.size(); ++right) {
+        while (seen.count(nums[right])) {
+            seen.erase(nums[left]);
+            current_sum -= nums[left];
+            left++;
+        }
+        seen.insert(nums[right]);
+        current_sum += nums[right];
+        max_sum = max(max_sum, current_sum);
+    }
+
+    return max_sum;
+}
