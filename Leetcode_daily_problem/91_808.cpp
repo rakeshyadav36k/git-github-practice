7fc8a648f238f81585873f92fa16a11a0651d21f
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-08-08 20:24:14 +0530

    using dp

--- Leetcode_daily_problem/91_808.cpp
+++ Leetcode_daily_problem/91_808.cpp
@@ -18,3 +18,30 @@ The process stops immediately after any turn in which one of the soups is used u
 
 Return the probability that A is used up before B, plus half the probability that both soups are used up in the same turn. Answers within 10-5 of the actual answer will be accepted.
  */
+class Solution {
+public:
+    double soupServings(int n) {
+        int m = ceil(n / 25.0);
+        unordered_map<int, unordered_map<int, double>> dp;
+
+        function<double(int, int)> calculateDP = [&](int i, int j) -> double {
+            return (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][j - 1] +
+                    dp[max(0, i - 2)][max(0, j - 2)] + dp[i - 1][max(0, j - 3)]) /
+                   4;
+        };
+
+        dp[0][0] = 0.5;
+        for (int k = 1; k <= m; k++) {
+            dp[0][k] = 1;
+            dp[k][0] = 0;
+            for (int j = 1; j <= k; j++) {
+                dp[j][k] = calculateDP(j, k);
+                dp[k][j] = calculateDP(k, j);
+            }
+            if (dp[k][k] > 1 - 1e-5) {
+                return 1;
+            }
+        }
+        return dp[m][m];
+    }
+};
