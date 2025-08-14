bddc55ef1affe3052370ce27908ea872f87c4839
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-08-14 23:00:11 +0530

    linear iteration

--- Leetcode_daily_problem/94_2264.cpp
+++ Leetcode_daily_problem/94_2264.cpp
@@ -10,3 +10,15 @@ Note:
 A substring is a contiguous sequence of characters within a string.
 There may be leading zeroes in num or a good integer.
  */
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        int result = -1;
+        for (int i = 0; i + 2 < num.length(); i++) {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
+                result = max(result, num[i] - '0');
+            }
+        }
+        return (result == -1) ? "" : string(3, '0' + result);
+    }
+};
