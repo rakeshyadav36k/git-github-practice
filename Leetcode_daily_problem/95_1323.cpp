16d56090a1a18673aaed84f4ce5a81dc1232e898
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-08-16 10:41:00 +0530

    iteration

--- Leetcode_daily_problem/95_1323.cpp
+++ Leetcode_daily_problem/95_1323.cpp
@@ -3,3 +3,16 @@
 
 Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
  */
+
+
+
+int maximum69Number (int num) {
+    string s = to_string(num);
+    for (char &c : s) {
+        if (c == '6') {
+            c = '9';
+            break;
+        }
+    }
+    return stoi(s);
+}
