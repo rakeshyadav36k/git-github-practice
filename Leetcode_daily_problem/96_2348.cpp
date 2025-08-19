15b5758bf23097d46ae0fffb475bc47e8839bd4a
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-08-19 21:42:22 +0530

    traversal

--- Leetcode_daily_problem/96_2348.cpp
+++ Leetcode_daily_problem/96_2348.cpp
@@ -3,3 +3,18 @@
 
 A subarray is a contiguous non-empty sequence of elements within an array.
  */
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long cnt = 0, streak = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                streak++;
+                cnt += streak;
+            } else {
+                streak = 0;
+            }
+        }
+        return cnt;
+    }
+};
