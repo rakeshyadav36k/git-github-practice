3f1a0becb854bb8b076bb51619182d6d9cabf7c0
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-08-27 21:43:22 +0530

    iteration

--- GFG_daily_problem/60_count_the_number_of_possible_triangles.cpp
+++ GFG_daily_problem/60_count_the_number_of_possible_triangles.cpp
@@ -3,3 +3,24 @@
  * Find the number of triangles that can be formed with three different array elements as lengths of three sides of the triangle. 
  * A triangle with three given sides is only possible if sum of any two sides is always greater than the third side.
  */
+int countTriangles(int arr[], int n) {
+    // Sort the array
+    sort(arr, arr + n);
+    int count = 0;
+
+    // Fix the third element
+    for (int k = 2; k < n; k++) {
+        int i = 0, j = k - 1;
+        while (i < j) {
+            // Check if arr[i] + arr[j] > arr[k]
+            if (arr[i] + arr[j] > arr[k]) {
+                // If yes, all elements from i to j can form a triangle with k
+                count += (j - i);
+                j--;
+            } else {
+                i++;
+            }
+        }
+    }
+    return count;
+}
