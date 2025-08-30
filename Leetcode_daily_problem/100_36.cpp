0e987a3926993d013119b8ab134133eed5e1fa5f
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-08-30 22:30:26 +0530

    brute force

--- Leetcode_daily_problem/100_36.cpp
+++ Leetcode_daily_problem/100_36.cpp
@@ -9,3 +9,47 @@ Note:
 A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 Only the filled cells need to be validated according to the mentioned rules.
  */
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isValidSudoku(vector<vector<char>>& board) {
+    // Check each row
+    for (int i = 0; i < 9; i++) {
+        unordered_set<char> seen;
+        for (int j = 0; j < 9; j++) {
+            if (board[i][j] != '.') {
+                if (seen.count(board[i][j])) return false;
+                seen.insert(board[i][j]);
+            }
+        }
+    }
+
+    // Check each column
+    for (int j = 0; j < 9; j++) {
+        unordered_set<char> seen;
+        for (int i = 0; i < 9; i++) {
+            if (board[i][j] != '.') {
+                if (seen.count(board[i][j])) return false;
+                seen.insert(board[i][j]);
+            }
+        }
+    }
+
+    // Check each 3x3 sub-box
+    for (int boxRow = 0; boxRow < 3; boxRow++) {
+        for (int boxCol = 0; boxCol < 3; boxCol++) {
+            unordered_set<char> seen;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    char curr = board[boxRow * 3 + i][boxCol * 3 + j];
+                    if (curr != '.') {
+                        if (seen.count(curr)) return false;
+                        seen.insert(curr);
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+}
