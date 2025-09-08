a391dc4314c7ac15009b80afb451fceacada2793
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-09-09 01:19:29 +0530

    enumeration

--- Leetcode_daily_problem/104_1317.cpp
+++ Leetcode_daily_problem/104_1317.cpp
@@ -7,3 +7,16 @@ a and b are No-Zero integers.
 a + b = n
 The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
  */
+
+ class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for(int a = 1; a < n; a++){
+            int b = n - a;
+            if((to_string(a) + to_string(b)).find('0') == string :: npos){
+                return {a, b};
+            }
+        }
+        return {};
+    }
+};
