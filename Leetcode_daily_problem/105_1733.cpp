37a1c636035d5382f305e5fce436bbc94890161a
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-09-10 21:29:10 +0530

    using set data structure

--- Leetcode_daily_problem/105_1733.cpp
+++ Leetcode_daily_problem/105_1733.cpp
@@ -11,3 +11,36 @@ You can choose one language and teach it to some users so that all friends can c
 Note that friendships are not transitive, meaning if x is a friend of y and y is a friend of z, this doesn't guarantee that x is a friend of z.
  
  */
+class Solution {
+public:
+    int minimumTeachings(int n, vector<vector<int>>& languages,
+                         vector<vector<int>>& friendships) {
+        unordered_set<int> cncon;
+        for (auto friendship : friendships) {
+            unordered_map<int, int> mp;
+            bool conm = false;
+            for (int lan : languages[friendship[0] - 1]) {
+                mp[lan] = 1;
+            }
+            for (int lan : languages[friendship[1] - 1]) {
+                if (mp[lan]) {
+                    conm = true;
+                    break;
+                }
+            }
+            if (!conm) {
+                cncon.insert(friendship[0] - 1);
+                cncon.insert(friendship[1] - 1);
+            }
+        }
+        int max_cnt = 0;
+        vector<int> cnt(n + 1, 0);
+        for (auto friendship : cncon) {
+            for (int lan : languages[friendship]) {
+                cnt[lan]++;
+                max_cnt = max(max_cnt, cnt[lan]);
+            }
+        }
+        return cncon.size() - max_cnt;
+    }
+};
